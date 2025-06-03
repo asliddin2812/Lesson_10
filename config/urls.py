588dc7bff77a95ea0ed9f99530cae5fed4279844
asliddin2812 <asliddin2812>
2025-06-03 16:59:23 +0500

    L10

--- config/urls.py
+++ config/urls.py
@@ -45,8 +45,6 @@ urlpatterns = [
    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('',include('app_silovar.urls')),
 ]
