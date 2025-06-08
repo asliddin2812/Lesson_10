758aca2fab8a25fd21f7dcb94f933d3dd0901d73
Asliddin Mamatov <169055620+asliddin2812@users.noreply.github.com>
2025-06-08 13:27:40 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ SWAGGER_SETTINGS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
