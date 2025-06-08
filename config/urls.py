5afaabbe7af40617391d89ebd983ad9166e383c5
Asliddin Mamatov <169055620+asliddin2812@users.noreply.github.com>
2025-06-08 16:50:42 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -32,6 +32,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('rest_framework.urls')),
     path('api/silovar/', include('app_silovar.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
@@ -39,6 +41,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
 
 if settings.DEBUG is False:
